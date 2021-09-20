cbe71a141eb43680d6a91319a51baa034baec561
Kaylee Martynow <kayleemartynow@creighton.edu>
2021-09-20 14:26:51 -0500

    Initial commit

---
+++ forms/forLoops/forLoops.js
@@ -0,0 +1,43 @@
+/*
+Loops are great if you want to do the same thing more than once such as  
+walk through items in an array and print them. 
+
+1. DO TOGETHER
+Use a for loop that outputs the numbers between 0 and 5, one per line  
+to the console.
+*/
+
+
+/* ***********************************
+2. CLASS ACTIVITY
+Output the even numbers between 1 and 10 to the console, one per line. 
+Use a for loop and an if statement.
+Hint: Mod operator (%): returns remainder of a division (vs quotient). 
+eg. 10 % 2 = 0  Explanation: 10/2 has no remainder so the 'mod' answer is 0.
+*/
+
+/*
+3. DO TOGETHER
+Use a for loop to output each item in the 'nums' array below to the console, 
+one per line. ** Pretend you don't know ahead of time how many 
+items are in the array.  
+*/
+
+/* ***********************************
+4. CLASS ACTIVITY
+Copy the code from the 'nums' example above and paste it below.
+Change the code so it SUMS UP the numbers in the nums array and 
+outputs the sum to the console after the loop completes. 
+Note: Answer should be 5005.
+*/
+
+    
+
+
+
+
+
+
+
+
+
---
+++ forms/whileLoops/whileLoops.js
@@ -0,0 +1,31 @@
+    
+/* ***************************************
+1. DO TOGETHER
+Write a while loop that counts by 1, starting at 1, 
+and doesn't stop until the user wants to stop. 
+eg. 1, 2, 3, 4, ......
+Hint: When do you want this loop to stop? You need a stopping 
+condition that changes inside the loop.
+*/
+
+
+/* ***********************************
+2. CLASS ACTIVITY
+You are entering employee names into a program over and over - but
+you don't know ahead of time how many new employees you have to enter until 
+you sit down to do it. So you need a program that adds names you enter until 
+you are done with all the names you have. 
+Use a While loop, an array named 'employees', if/else, and .push(). 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
